c5e20dc7ac93d1ca076c3dd405737a1a8bd78c0c
doom-9 <110001160+doom-9@users.noreply.github.com>
2023-06-05 20:23:13 +0800

    初始化

---
+++ forge.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  packagerConfig: {
+    osxSign: {
+      identity:
+        "Developer ID Application: Root Sports Investment (Beijing) Co., Ltd.",
+    },
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-dmg",
+      config: {},
+    },
+  ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "doom-9",
+          name: "mail",
+        },
+        draft: true,
+        prerelease: false,
+      },
+    },
+  ],
+};
