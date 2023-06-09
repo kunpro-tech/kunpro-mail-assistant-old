a63fe67dae08f6afa003944cd4258029200b3626
doom-9 <110001160+doom-9@users.noreply.github.com>
2023-06-09 11:39:05 +0800

    increasePackagingType

--- forge.config.js
+++ forge.config.js
@@ -15,6 +15,10 @@ module.exports = {
       name: "@electron-forge/maker-dmg",
       config: {},
     },
+    {
+      name: '@electron-forge/maker-wix',
+      config: {}
+    }
   ],
   publishers: [
     {
